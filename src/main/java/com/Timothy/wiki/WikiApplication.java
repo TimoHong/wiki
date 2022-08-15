b7e02b98266173921aea960ac71a5e4924d4c08d
TimoHong <105810558+TimoHong@users.noreply.github.com>
2022-08-15 17:38:21 +0800

    2-6 Hello World controller

--- src/main/java/com/Timothy/wiki/WikiApplication.java
+++ src/main/java/com/Timothy/wiki/WikiApplication.java
@@ -4,8 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
+@ComponentScan("com.Timothy")
 @SpringBootApplication
 public class WikiApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
