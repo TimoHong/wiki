ea78e9faae9252e0119bb9d9b66ca654f1a797a3
TimoHong <105810558+TimoHong@users.noreply.github.com>
2022-08-17 14:03:20 +0800

    3-5 集成mybatis generator

--- doc/all.sql
+++ doc/all.sql
@@ -7,3 +7,13 @@ create table `test` (
 )engine=innodb default charset=utf8mb4 comment='test'
 
 insert into `test` (id, name, password) values (1, 'test', 'password');
+
+drop table if exists `test`;
+create table `test` (
+  `id` bigint not null comment 'id',
+  `name` varchar(50) comment 'name',
+  `password` varchar(50) comment 'password',
+  primary key(`id`)
+)engine=innodb default charset=utf8mb4 comment='test'
+
+insert into `test` (id, name, password) values (1, 'test', 'password');
--- src/main/java/com/Timothy/wiki/mapper/TestMapper.java
+++ src/main/java/com/Timothy/wiki/mapper/TestMapper.java
@@ -5,5 +5,5 @@ import com.Timothy.wiki.domain.Test;
 import java.util.List;
 
 public interface TestMapper {
-    public List<Test> list();
+    List<Test> list();
 }
