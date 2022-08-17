e0484419a5dd356bce8448c1a788c48263e8d83b
TimoHong <105810558+TimoHong@users.noreply.github.com>
2022-08-18 00:38:11 +0800

    3-6 adding CommonResp

--- src/main/java/com/Timothy/wiki/controller/EbookController.java
+++ src/main/java/com/Timothy/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.Timothy.wiki.controller;
 
 import com.Timothy.wiki.domain.Ebook;
+import com.Timothy.wiki.resp.CommonResp;
 import com.Timothy.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list= ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
