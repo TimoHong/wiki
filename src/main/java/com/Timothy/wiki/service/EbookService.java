c3823db16ae47a1cabf4396f9ee12e565029cc17
TimoHong <105810558+TimoHong@users.noreply.github.com>
2022-08-18 01:03:59 +0800

    3-7 封裝request參數和return參數

--- src/main/java/com/Timothy/wiki/service/EbookService.java
+++ src/main/java/com/Timothy/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.Timothy.wiki.service;
 
 import com.Timothy.wiki.domain.Ebook;
+import com.Timothy.wiki.domain.EbookExample;
 import com.Timothy.wiki.mapper.EbookMapper;
+import com.Timothy.wiki.req.EbookReq;
+import com.Timothy.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,7 +17,18 @@ public class EbookService {
     @Autowired
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%"+req.getName()+"%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook: ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            respList.add(ebookResp);
+
+        }
+        return respList;
     }
 }
